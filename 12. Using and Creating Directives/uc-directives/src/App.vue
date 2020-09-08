65917aae55dfeb8cc9b302ea65fc7ca6cbc34a2e
SokolovPE <SokolovP@outlook.com>
2020-09-08 23:09:43 +0300

    172. Registering Directives Locally

--- 12. Using and Creating Directives/uc-directives/src/App.vue
+++ 12. Using and Creating Directives/uc-directives/src/App.vue
@@ -16,13 +16,34 @@
             >
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed="'red'">
+                    Color this too
+                </p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    directives: {
+        'local-highlight': {
+            bind(el, binding, vnode) {
+                var delay = 0;
+                if (binding.modifiers['delayed']) {
+                    delay = 3000;
+                }
+                setTimeout(function() {
+                    if (binding.arg == 'background') {
+                        el.style.backgroundColor = binding.value;
+                    } else {
+                        el.style.color = binding.value;
+                    }
+                }, delay);
+            }
+        }
+    }
+};
 </script>
 
 <style></style>
