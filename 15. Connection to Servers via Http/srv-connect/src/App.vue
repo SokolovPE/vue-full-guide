d28f3c7d51a9fad53fa00af7e6250f010d902bed
SokolovPE <SokolovP@outlook.com>
2020-11-10 23:08:48 +0300

    POSTing Data to a Server (Sending a POST Request)

--- 15. Connection to Servers via Http/srv-connect/src/App.vue
+++ 15. Connection to Servers via Http/srv-connect/src/App.vue
@@ -1,28 +1,60 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+        <div class="row">
+            <div
+                class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+            >
+                <h1>Http</h1>
+                <div class="from-group">
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="user.username"
+                    />
+                </div>
+                <div class="from-group">
+                    <label>Mail</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="user.email"
+                    />
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-    name: 'App',
-    components: {
-        HelloWorld
+    data() {
+        return {
+            user: {
+                username: '',
+                email: ''
+            }
+        };
+    },
+    methods: {
+        submit() {
+            this.$http
+                .post(
+                    'https://vue-http-f059c.firebaseio.com/data.json',
+                    this.user
+                )
+                .then(
+                    response => {
+                        console.log(response);
+                    },
+                    error => {
+                        console.log(error);
+                    }
+                );
+        }
     }
 };
 </script>
 
-<style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
+<style></style>
