f55f7b9813bdbfd3f0366904ba95d507ce84363a
SokolovPE <SokolovP@outlook.com>
2020-11-16 00:30:20 +0300

    582. Loading Routes Lazily

--- 16. Routing in a VueJS Application/vue-routing/src/routes.js
+++ 16. Routing in a VueJS Application/vue-routing/src/routes.js
@@ -1,9 +1,49 @@
-import User from '@/components/user/User.vue';
-import UserStart from '@/components/user/UserStart.vue';
-import UserDetail from '@/components/user/UserDetail.vue';
-import UserEdit from '@/components/user/UserEdit.vue';
 import Home from '@/components/Home.vue';
 import Header from '@/components/Header.vue';
+
+//* Webpack will recognize wthis syntax to lazy load parts.
+// Whenever we want to laod something in ensure's firest argument,
+// execute function from second parameter, and resolves the path we should really use.
+// So those parts will be loaded when they really needed.
+const User = resolve => {
+    require.ensure(
+        ['@/components/user/User.vue'],
+        () => {
+            resolve(require('@/components/user/User.vue'));
+        },
+        'user'
+    );
+};
+
+const UserStart = resolve => {
+    require.ensure(
+        ['@/components/user/UserStart.vue'],
+        () => {
+            resolve(require('@/components/user/UserStart.vue'));
+        },
+        'user'
+    );
+};
+
+const UserDetail = resolve => {
+    require.ensure(
+        ['@/components/user/UserDetail.vue'],
+        () => {
+            resolve(require('@/components/user/UserDetail.vue'));
+        },
+        'user'
+    );
+};
+
+const UserEdit = resolve => {
+    require.ensure(
+        ['@/components/user/UserEdit.vue'],
+        () => {
+            resolve(require('@/components/user/UserEdit.vue'));
+        },
+        'user'
+    );
+};
 
 export const routes = [
     {
