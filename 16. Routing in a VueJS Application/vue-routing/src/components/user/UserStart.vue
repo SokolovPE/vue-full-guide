6958c922db68a95e7c30e9a5429d27924e0a770f
SokolovPE <SokolovP@outlook.com>
2020-11-15 22:27:43 +0300

    569. Navigating to Nested Routes

--- 16. Routing in a VueJS Application/vue-routing/src/components/user/UserStart.vue
+++ 16. Routing in a VueJS Application/vue-routing/src/components/user/UserStart.vue
@@ -3,9 +3,29 @@
         <p>Please select a User</p>
         <hr />
         <ul class="list-group">
-            <li class="list-group-item" style="cursor: pointer">User 1</li>
-            <li class="list-group-item" style="cursor: pointer">User 2</li>
-            <li class="list-group-item" style="cursor: pointer">User 3</li>
+            <router-link
+                class="list-group-item"
+                style="cursor: pointer"
+                v-for="user in users"
+                :key="user.id"
+                :to="'/user/' + user.id"
+                tag="li"
+                >User {{ user.id }}</router-link
+            >
         </ul>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: [
+                { id: 1, name: 'User 1' },
+                { id: 2, name: 'User 2' },
+                { id: 3, name: 'User 3' }
+            ]
+        };
+    }
+};
+</script>
