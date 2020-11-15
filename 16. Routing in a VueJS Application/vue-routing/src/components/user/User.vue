5354a938685be75a45c44ca9b6289fdd5e109828
SokolovPE <SokolovP@outlook.com>
2020-11-15 21:38:24 +0300

    563. Navigating from Code (Imperative Navigation)

--- 16. Routing in a VueJS Application/vue-routing/src/components/user/User.vue
+++ 16. Routing in a VueJS Application/vue-routing/src/components/user/User.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+        <h1>The User Page</h1>
+        <hr />
+        <!-- This is how it's done by button with handler. -->
+        <button class="btn btn-primary" @click="navigateToHome">
+            Go to Home
+        </button>
+        <hr />
+        <!-- This is how it's dont by router-link. -->
+        <router-link to="/" tag="button" class="btn btn-primary"
+            >Go to home too</router-link
+        >
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            this.$router.push('/');
+        }
+    }
+};
+</script>
