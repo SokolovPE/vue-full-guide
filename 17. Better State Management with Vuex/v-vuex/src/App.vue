c9a317cc68d97df7f3d2bc96e39af12423841609
SokolovPE <SokolovP@outlook.com>
2020-11-16 23:10:33 +0300

    588. Using the Centralized State

--- 17. Better State Management with Vuex/v-vuex/src/App.vue
+++ 17. Better State Management with Vuex/v-vuex/src/App.vue
@@ -5,9 +5,9 @@
                 class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
             >
                 <h1>Vuex</h1>
-                <app-result :counter="counter"></app-result>
+                <app-result></app-result>
                 <hr />
-                <app-counter @updated="counter += $event"></app-counter>
+                <app-counter></app-counter>
             </div>
         </div>
     </div>
@@ -18,11 +18,6 @@ import Counter from '@/components/Counter.vue';
 import Result from '@/components/Result.vue';
 
 export default {
-    data() {
-        return {
-            counter: 0
-        };
-    },
     components: {
         appCounter: Counter,
         appResult: Result
